cd25cb5b6a0985469b29f3f2e14dfbe085e8bae9
Paulo Choi <makwanza@gmail.com>
2015-09-22 15:53:12 -0400

    First Commit

---
+++ BlocTalk/BlocTalk/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlocTalk
+//
+//  Created by Paulo Choi on 9/22/15.
+//  Copyright (c) 2015 Paulo Choi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlocTalk/BlocTalk/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlocTalk
+//
+//  Created by Paulo Choi on 9/22/15.
+//  Copyright (c) 2015 Paulo Choi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
