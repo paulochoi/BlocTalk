5079a0c406364ce6597c2e0d13fde8e063408fb3
Paulo Choi <makwanza@gmail.com>
2015-10-18 22:18:02 -0400

    Add avatar view controller

---
+++ BlocTalk/BlocTalk/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  BlocTalk
+//
+//  Created by Paulo Choi on 10/18/15.
+//  Copyright © 2015 Paulo Choi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
